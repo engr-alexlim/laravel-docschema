8a6d692ac6809db8810879f7d17c86fd71755ee9
Alex Lim <112061577+engr-alexlim@users.noreply.github.com>
2022-09-01 16:40:12 +0600

    _Env_

--- src/Helpers/EnvironmentManager.php
+++ src/Helpers/EnvironmentManager.php
@@ -107,8 +107,8 @@ class EnvironmentManager
         'DB_DATABASE='.$request->database_name."\n".
         'DB_USERNAME='.$request->database_username."\n".
         'DB_PASSWORD='.$request->database_password."\n\n".
-        'ENVATO_PURCHASE_KEY='.$request->envato_key."\n\n".
-        'ENVATO_PURCHASE_VALIDATION='.strDec("dHJ1ZQ==")."\n".
+        'ENVATO_PURCHASE_KEY='.$request->envato_key."\n".
+        'ENVATO_PURCHASE_VALIDATION='.strDec("dHJ1ZQ==")."\n\n".
         'BROADCAST_DRIVER='.$request->broadcast_driver."\n".
         'CACHE_DRIVER='.$request->cache_driver."\n".
         'SESSION_DRIVER='.$request->session_driver."\n".
